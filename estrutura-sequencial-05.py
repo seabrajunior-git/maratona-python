bce8f593d51dfdd1754773ed757bf90511b03c58
seabrajunior-git <39681384+seabrajunior-git@users.noreply.github.com>
2019-01-07 16:19:34 -0200

    Create estrutura-sequencial-05.py
    solução exercício 05

---
+++ estrutura-sequencial-05.py
@@ -0,0 +1,4 @@
+# Estrutura Sequnecial Exercício 05
+metros = float(input("Digite o valor a ser convertido em centímetros: "))
+centimetros = metros*100
+print("{:.2f} metros equivale a {:.2f} centímetros".format(metros, centimetros))
